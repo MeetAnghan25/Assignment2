ea2d1118887fc9c26d2a7d7efa76c18d9f1b193b
Meet Anghan <anghanmeet2506@gmail.com>
2022-02-09 22:47:37 -0500

    Initial Commit

---
+++ Meet_Anghan_A2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Meet_Anghan_A2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
